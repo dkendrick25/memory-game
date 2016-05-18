89dac22db28734562a6097f0294a736db2bb4bfd
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-05-18 11:07:43 -0400

    added link

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a memory game using jQuery
+
+[Check it out](http://kendrickmemorygame.surge.sh/)
--- memory_game.js
+++ memory_game.js
@@ -1,6 +1,9 @@
+var state;
+
 $(function() {
   //when click add to class to reveal the card in css
   $('.tile').click(function() {
+    state = 'first';
     $(this).addClass('open');
-  })
+  });
 });
